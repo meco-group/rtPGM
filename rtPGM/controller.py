08d4f6943fd9888b680b4f34313bb742644e0184
rubenvanparys <ruben.vanparys@kuleuven.be>
2018-04-06 16:16:20 +0200

    added license

--- rtPGM/controller.py
+++ rtPGM/controller.py
@@ -1,3 +1,23 @@
+# This file is part of rtPGM.
+#
+# rtPGM -- real-time Proximal Gradient Method
+# Copyright (C) 2018 Ruben Van Parys, KU Leuven.
+# All rights reserved.
+#
+# OMG-tools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
 import numpy as np
 import scipy.linalg
 import scipy.signal
